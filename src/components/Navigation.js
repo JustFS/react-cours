10dbe4e84345cbae1c459709f78a4cb763871711
Julien Az <julien.azbrg@gmail.com>
2021-04-01 15:32:02 +0200

    cours 1

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
   return (
     <div className="nav">
       <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
+        <NavLink exact to="/" activeClassName="nav-active">
+          Home
+        </NavLink>
+        <NavLink to="/about" activeClassName="nav-active">
+          About
+        </NavLink>
+        <NavLink to="/contact" activeClassName="nav-active">
+          Contact
+        </NavLink>
       </ul>
     </div>
   );
