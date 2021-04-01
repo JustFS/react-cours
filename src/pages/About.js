92156a09041b99f8c2373d1934478dfbac1f00a2
Julien Az <julien.azbrg@gmail.com>
2021-04-01 16:29:33 +0200

    added edit

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,78 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../components/Logo";
 import Navigation from "../components/Navigation";
 
 const About = () => {
+  const [textEdit, setTextEdit] = useState(false);
+  const [text, setText] = useState("Coucou les amis");
+
   return (
     <div className="about">
       <Logo />
       <Navigation />
       <h1>A PROPOS</h1>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas
-        numquam officia quis reiciendis dolor perspiciatis dicta perferendis
-        maxime dolorum quam iste laboriosam, inventore dignissimos, magni culpa
-        ullam placeat harum facere?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe dolore
-        magni commodi ullam iusto enim sint modi quo in ipsam laboriosam, nam
-        similique temporibus quos doloremque corrupti at amet reprehenderit.
-        Suscipit facilis hic, reiciendis eligendi ducimus praesentium!
-        Provident, magni saepe aliquid, eius et aliquam nobis, voluptatibus
-        nihil cumque vel magnam beatae dignissimos quam expedita nesciunt
-        accusamus? Perferendis temporibus rem tempora odio nesciunt, officia
-        error neque assumenda itaque quisquam dolore, nostrum mollitia maiores
-        architecto aspernatur suscipit omnis tempore aliquam, esse tenetur saepe
-        a unde? Non, atque distinctio? Obcaecati labore perferendis autem itaque
-        aspernatur quia odit, error sequi consequuntur, enim tenetur, ab quaerat
-        fugiat id voluptatum vitae perspiciatis ipsa reiciendis beatae ducimus
-        distinctio officiis non porro! Corporis provident nam similique
-        blanditiis velit! Voluptatibus blanditiis autem inventore quam obcaecati
-        ratione, voluptas numquam quidem. Nemo, tempora, corrupti adipisci sint
-        reprehenderit illum quasi qui necessitatibus repellat suscipit, esse id
-        architecto voluptatem facilis nihil. Iure quos molestias eius, deleniti
-        placeat blanditiis inventore molestiae ratione libero quis ullam
-        suscipit obcaecati provident saepe temporibus. Nihil, magni rerum
-        inventore expedita totam explicabo facilis perferendis eum error saepe
-        assumenda nemo soluta accusamus suscipit consequuntur quibusdam, optio
-        perspiciatis omnis mollitia? Necessitatibus, quis voluptatum repudiandae
-        quo similique sed cupiditate, iste quas laudantium fugit nam iusto
-        repellendus alias in magnam deleniti. Ex fugit provident vitae magnam
-        voluptatem quidem ipsum error optio, possimus eaque similique eveniet et
-        corrupti quisquam culpa nostrum aliquam dignissimos quibusdam
-        consectetur quasi eum dolorem quas! Cumque, eaque suscipit. Iure
-        exercitationem quisquam obcaecati. Eum ullam eveniet illo error! Debitis
-        itaque dolorem, autem placeat ipsam dignissimos minima magni, natus
-        ducimus iusto nam praesentium possimus molestias iste? Itaque facere
-        ducimus iusto molestias, nisi quisquam expedita est fuga minus porro
-        nemo nesciunt natus eveniet vero id laborum veniam consectetur
-        aspernatur nam. Iure asperiores tenetur modi voluptatum doloremque
-        explicabo debitis minima ratione incidunt, nesciunt necessitatibus
-        facere mollitia labore, totam dolorum eligendi? Doloremque magnam sint,
-        eveniet nihil consequuntur ratione. Voluptates voluptatibus temporibus
-        rerum autem, dolor dolore harum saepe quos aut, architecto, eveniet
-        praesentium sit quae. Eum, aliquid mollitia! Voluptatem repudiandae
-        dicta repellat illum voluptate quam illo harum assumenda! Delectus,
-        esse, quidem, distinctio autem pariatur tempora ipsum cum praesentium
-        aliquam fugit provident magnam! Dignissimos officia aliquid illum
-        molestiae minus. Accusantium aliquam eius eum molestiae, vero, omnis ex
-        eveniet ab iusto id labore consequuntur aperiam, quo assumenda
-        praesentium laudantium incidunt voluptatibus in? Suscipit nisi quaerat
-        iusto? Quos, quod neque ipsum repellendus obcaecati minima nihil iusto
-        similique esse, labore veniam molestias corporis perspiciatis blanditiis
-        recusandae sint ipsa consequatur vel eos, debitis explicabo rerum. Ex
-        sit nemo hic facere ea illo magnam! Numquam itaque repellat atque odio.
-        Laudantium maxime facilis quae impedit odio repudiandae doloremque,
-        architecto exercitationem, dicta ratione accusamus dolore tempore!
-        Consequuntur, eveniet voluptate ipsam dolore sequi sunt ea totam
-        veritatis quo. Nobis error maxime ullam aspernatur! Atque neque tenetur
-        dolorem optio qui praesentium mollitia libero ea ipsam! Accusantium
-        quaerat cum sint expedita? Quibusdam consectetur assumenda officiis
-        vero. Accusantium alias ullam, ut sunt nam nemo similique voluptate illo
-        dolor incidunt perspiciatis amet, deleniti dolores odit earum possimus
-        doloremque explicabo.
-      </p>
+      <button onClick={() => setTextEdit(!textEdit)}>Editer</button>
+
+      {textEdit ? (
+        <>
+          <textarea
+            onChange={(e) => setText(e.target.value)}
+            defaultValue={text}
+            cols="40"
+            rows="10"
+          ></textarea>
+          <button onClick={() => setTextEdit(false)}>
+            Valider modification
+          </button>
+        </>
+      ) : (
+        <p>{text}</p>
+      )}
     </div>
   );
 };
