dbf69aa02dfab3afdc2df221a0a086b146695b2a
Julien Az <julien.azbrg@gmail.com>
2021-04-07 15:18:44 +0200

    finished

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -4,18 +4,41 @@ import Card from "./Card";
 
 const Countries = () => {
   const [countriesData, setCountriesData] = useState([]);
+  const [sortedCountries, setSortedCountries] = useState([]);
   const [response, setResponse] = useState("");
+  const [playOnce, setPlayOnce] = useState(true);
+
+  const [rangeValue, setRangeValue] = useState(40);
+  const [selectedRadio, setSelectedRadio] = useState("");
+  const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
 
   // joue le useEffect quand le composant se monte
   useEffect(() => {
-    axios
-      .get(
-        "https://restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag"
-      )
-      .then((res) => setCountriesData(res.data));
+    if (playOnce) {
+      axios
+        .get(
+          "https://restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag"
+        )
+        .then((res) => {
+          setCountriesData(res.data);
+          setPlayOnce(false);
+        });
+    }
+
+    const sortedCountry = () => {
+      const countryObj = Object.keys(countriesData).map(
+        (i) => countriesData[i]
+      );
+      const sortedArray = countryObj.sort((a, b) => {
+        return b.population - a.population;
+      });
+      sortedArray.length = rangeValue;
+      setSortedCountries(sortedArray);
+    };
+    sortedCountry();
 
     // rejoue le useEffect quand le callback [] est appelé
-  }, []);
+  }, [rangeValue, playOnce, countriesData]);
 
   return (
     <div className="countries">
@@ -24,12 +47,47 @@ const Countries = () => {
         onChange={(e) => setResponse(e.target.value)}
         type="text"
         placeholder="Entrez le nom d'un pays"
+        value={response}
       />
+      <br />
+      {response && (
+        <button onClick={() => setResponse("")}>Annuler recherche</button>
+      )}
+
+      <ul className="radios">
+        {radios.map((continent) => {
+          return (
+            <li key={continent}>
+              <input
+                name="cont"
+                type="radio"
+                value={continent}
+                id={continent}
+                onChange={(e) => setSelectedRadio(e.target.value)}
+              />
+              <label htmlFor={continent}>{continent}</label>
+            </li>
+          );
+        })}
+      </ul>
+
+      <div className="range">
+        <input
+          type="range"
+          min="1"
+          max="250"
+          onChange={(e) => setRangeValue(e.target.value)}
+          value={rangeValue}
+        />
+        {rangeValue}
+      </div>
+
       <ul>
-        {countriesData
+        {sortedCountries
           .filter((country) =>
             country.name.toLowerCase().includes(response.toLowerCase())
           )
+          .filter((country) => country.region.includes(selectedRadio))
           .map((country, index) => (
             <Card country={country} key={index} />
           ))}
