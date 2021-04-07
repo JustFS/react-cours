a38e538f64f85d3d98935b1fdf022c6381e1160f
Julien Az <julien.azbrg@gmail.com>
2021-04-07 11:49:53 +0200

    add card component

---
+++ src/components/Card.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Card = (props) => {
+  const { country } = props;
+
+  const transformNumber = (number) => {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+  };
+
+  return (
+    <li className="card">
+      <h3>{country.name}</h3>
+      <img src={country.flag} alt={"drapeau de " + country.name} />
+      <h4>{country.capital}</h4>
+      <p>Pop : {transformNumber(country.population)}</p>
+    </li>
+  );
+};
+
+export default Card;
--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Card from "./Card";
 
 const Countries = () => {
-  axios
-    .get(
-      "https://restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag"
-    )
-    .then((res) => console.log(res.data));
+  const [countriesData, setCountriesData] = useState([]);
+
+  // joue le useEffect quand le composant se monte
+  useEffect(() => {
+    axios
+      .get(
+        "https://restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag"
+      )
+      .then((res) => setCountriesData(res.data));
+
+    // rejoue le useEffect quand le callback [] est appelé
+  }, []);
 
   return (
-    <div>
+    <div className="countries">
       <h1>Countries</h1>
+      <ul>
+        {countriesData.map((country, index) => (
+          <Card country={country} key={index} />
+        ))}
+      </ul>
     </div>
   );
 };
