e262f51f8be65e8578ba103d3357f3bf689f70f2
Julien Az <julien.azbrg@gmail.com>
2022-04-11 09:23:23 +0200

    J2 matin

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,18 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import Logo from "../components/Logo";
 import Navigation from "../components/Navigation";
 
 const Home = () => {
+  const [countriesData, setCountriesData] = useState([]);
+
+  // Lancer une action quand le composant se monte
+  useEffect(() => {
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((res) => console.log(res));
+  }, []);
+
   return (
     <div className="home-container">
       <Logo />
